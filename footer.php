a666a581ca06497a5a3275102d4e22275917c59c
afonsoinfo <afonsoinfo@gmail.com>
2012-10-23 23:21:23 -0200

    new version of jquery

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@
 </div><!-- fecha #page -->
 
 <!-- jQuery Fallback -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.8.2.min.js"><\/script>')</script>
 
 <!-- scripts concatenated and minified via ant build script-->
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/controller.js"></script>
